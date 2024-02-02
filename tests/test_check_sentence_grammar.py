bdb85ecd31b7d1c6d403852c39426e0b117ea654
samneat <samjneat@gmail.com>
2024-02-02 09:42:09 +0000

    added all tests

--- tests/test_check_sentence_grammar.py
+++ tests/test_check_sentence_grammar.py
@@ -23,3 +23,6 @@ def test_with_capital_letter_and_exclamation_mark():
 def test_with_capital_letter_and_comma():
     result = check_sentence_grammar("Hello, this is a fine day,")
     assert result == False
+
+def test_start_with_lowercase_letter_and_valid_punctuation():
+    result = check_sentence_grammar("hello, this is a fine day.")
